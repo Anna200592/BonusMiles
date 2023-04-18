4cc7a88ae3b6ac6b71a743918dbb58c895378e17
Neto test <strokova-92@mail.ru>
2023-04-18 22:19:09 +0300

    New commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,8 +2,8 @@ public class BonusMilesService {
     public int calculate(int price) {
 
         if (price < 0) {
-           price=0;
+            price = 0;
         }
-        return price /20;
+        return price / 20;
     }
 }
